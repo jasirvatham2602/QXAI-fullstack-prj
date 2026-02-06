b32b094777e2dd115f8094646e9485718c96ab87
Josh A <josha@Joshs-Laptop.attlocal.net>
2026-02-05 22:13:34 -0500

    chore: update About.jsx

--- src/About.jsx
+++ src/About.jsx
@@ -14,7 +14,7 @@ function About() {
         <>
             <h1>About</h1>   
             <p>
-                This project aims to create a easy-to-use UI interface for neurologists to use and test the QXAI and XAI models. The MRI scan should be a T1-weighted MRI scan in the axial view of the brain for proper classification of AD and CONTROL and must be T2-weighted MRI scan in axial view for proper PD diagnosis. The code to train the model produces different performance due to not having a specified seed during the stratified sampling of the dataset into train, test, and validation data; one result of the model can be seen on <a href="https://github.com/jasirvatham2602/QXAI-prj" target='_blank'>QXAI-prj Github Link</a>. Since these models weren't saved, the models had to be retrained and may have slightly different performance. The code for the fullstack website can be seen on <a href="https://github.com/jasirvatham2602/QXAI-fullstack-prj">QXAI-fullstack-prj Github Link</a>. The diagnosis of the QXAI and XAI models may be incorrect; therefore, the sole use of this site in diagnosis of a patient for a neurological disorder is not recommended, as it risks the persons life. Neurologists should only use this site and other AI tools as an aid, and should make the final diagnosis. Overall, this project helps create trust between both Quantum and Classical AI models in healthcare and improves usability of AI in healthcare.
+                This project aims to create a easy-to-use UI interface for neurologists to use and test the QXAI and XAI models. The MRI scan should be a T1-weighted MRI scan in the axial view of the brain for proper classification of AD and CONTROL and must be T2-weighted MRI scan in axial view for proper PD diagnosis. The code to train the model produces different performance due to not having a specified seed during the stratified sampling of the dataset into train, test, and validation data; one result of the model can be seen on <a href="https://github.com/jasirvatham2602/QXAI-prj" target='_blank'>QXAI-prj Github Link</a>. Since these models weren't saved, the models had to be retrained and may have slightly different performance. The code for the fullstack website can be seen on <a href="https://github.com/jasirvatham2602/QXAI-fullstack-prj">QXAI-fullstack-prj Github Link</a>. The diagnosis of the QXAI and XAI models may be incorrect; therefore, the sole use of this site in diagnosis of a patient for a neurological disorder is not recommended, as it risks the person's life. Neurologists should only use this site and other AI tools as an aid, and should make the final diagnosis. Overall, this project helps create trust between both Quantum and Classical AI models in healthcare and improves usability of AI in healthcare.
             
             </p>
             <p>
@@ -22,12 +22,7 @@ function About() {
             </p>
             <p>
                    The use of Quantum and Classical Saliency Maps help improves interpretability of the AI models; neurologists can see why each model produces a certain diagnosis, helping them determine whether to trust the model or not. The UI interface not only allows other researchers to test the AI models, but also helps bring AI into healthcare facilities, with the goal to provide both fast and accurate diagnoses. 
-            </p>
-            {/* <ReactMarkdown remarkPlugins={[remarkGfm, remarkMath]} rehypePlugins={rehypeKatex} skipHtml={false}
-            components={{math: ({value}) => <span>{value}</span>}}
-            > 
-                {content}
-            </ReactMarkdown> */}
+            </p> 
         </>
     );
 }
